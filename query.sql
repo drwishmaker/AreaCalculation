bc4da336e4ac5cea6a836e0d69d69c11126a8f0f
Artemy Krupsky <56369600+drwishmaker@users.noreply.github.com>
2022-10-30 14:02:54 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -17,12 +17,11 @@ CREATE TABLE ProductCategories (
 );
 
 INSERT INTO Products VALUES (1, 'Pencil');
-INSERT INTO Products VALUES	(2, 'CLR via C#');
-INSERT INTO Products VALUES	(3, 'Copybook');
+INSERT INTO Products VALUES (2, 'CLR via C#');
+INSERT INTO Products VALUES (3, 'Copybook');
 INSERT INTO Products VALUES (4, 'marmalade');
 
-INSERT INTO Categories VALUES	(1, 'Book');
-
+INSERT INTO Categories VALUES (1, 'Book');
 INSERT INTO Categories VALUES (2, 'Office supplies');
 
 INSERT INTO ProductCategories VALUES (2, 1);
